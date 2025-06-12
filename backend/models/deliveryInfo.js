128e3cb84c61f1087745fff9d60edcd7d5124e0b
Francismo2 <francislingan.onlineclass.94@gmail.com>
2025-06-12 11:16:36 +0800

    Finalized Sequelize

---
+++ backend/models/deliveryInfo.js
@@ -0,0 +1,122 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/sequelize.js';
+
+const DeliveryInfo = sequelize.define(
+  'DeliveryInfo',
+  {
+    ID: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    deliveryId: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    customerId: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: 'customer',
+        key: 'ID',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    provinceId: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: 'provinces',
+        key: 'ID',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    cityId: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: 'cities',
+        key: 'ID',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    barangayId: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: 'barangays',
+        key: 'ID',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    country: {
+      type: DataTypes.STRING(11),
+      allowNull: false,
+      defaultValue: 'Philippines',
+    },
+    detailedAddress: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    zipCode: {
+      type: DataTypes.STRING(4),
+      allowNull: false,
+    },
+    contactNumber: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    CNVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    updateAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: 'deliveryInfo',
+    timestamps: false,
+  }
+);
+
+// Associations
+DeliveryInfo.associate = (models) => {
+  DeliveryInfo.belongsTo(models.Customer, {
+    foreignKey: 'customerId',
+    as: 'customer',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  });
+
+  DeliveryInfo.belongsTo(models.Province, {
+    foreignKey: 'provinceId',
+    as: 'province',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  });
+
+  DeliveryInfo.belongsTo(models.City, {
+    foreignKey: 'cityId',
+    as: 'city',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  });
+
+  DeliveryInfo.belongsTo(models.Barangay, {
+    foreignKey: 'barangayId',
+    as: 'barangay',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  });
+};
+
+export default DeliveryInfo;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import 'dotenv/config'
 import { connectToDatabase } from './config/sequelize.js';
 import connectCloudinary from './config/cloudinary.js';
 
-
 // APP CONFIG
 const app = express();
 const port = process.env.PORT || 5001;
@@ -17,12 +16,11 @@ app.use(cors());
 
 
 // API ENDPOINTS
-
-
 app.get('/', (req, res) => {
     res.send("API Working")
 }) 
 
+// START SERVER
 const startServer = async () => {
     try {
         await connectToDatabase();
