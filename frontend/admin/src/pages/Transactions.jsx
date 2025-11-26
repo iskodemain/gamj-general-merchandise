cf8f39cc074acc94d11f68fd7004ab950f691806
Francismo2 <francislingan.onlineclass.94@gmail.com>
2025-11-26 16:03:19 +0800

    fix(ui): update sidebar styling and correct visual inconsistencies

---
+++ frontend/admin/src/pages/Transactions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Transactions.css"
+
+const Transactions = () => {
+  return (
+    <div>
+      <h1>Order Transaction</h1>
+    </div>
+  )
+}
+
+export default Transactions
