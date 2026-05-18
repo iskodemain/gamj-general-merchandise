56869be7e1d8f5dc13dd8f3788afea6b3211f94d
Francis Lingan <francislingan.onlineclass.94@gmail.com>
2026-05-18 13:00:18 +0800

    Uncomment allowed origin for Vercel app

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   // "https://www.gamjgeneralmerchandise.store",
-  // "https://gamj-general-merchandise-customer.vercel.app",
+  "https://gamj-general-merchandise-customer.vercel.app",
   "https://gamj-general-merchandise-admin.vercel.app"
 ];
 
